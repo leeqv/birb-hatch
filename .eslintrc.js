c8e468059d904559e2a855e9306bb71afbcd45da
Lee Valenzuela <alqvalenzuela@gmail.com>
2023-08-20 14:38:40 +0800

    Use eslint, prettier. Remove unused files

---
+++ .eslintrc.js
@@ -0,0 +1,33 @@
+module.exports = {
+  env: {
+    browser: true,
+    commonjs: true,
+    es2021: true,
+  },
+  extends: ["eslint:recommended", "plugin:react/recommended", "prettier"],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2018,
+    sourceType: "module",
+  },
+  plugins: ["react", "prettier"],
+  rules: {
+    indent: ["error", 2],
+    "linebreak-style": ["error", "unix"],
+    quotes: ["error", "double"],
+    semi: ["error", "always"],
+    "no-trailing-spaces": "error",
+    "object-curly-spacing": ["error", "always"],
+    "arrow-spacing": ["error", { before: true, after: true }],
+    "react/prop-types": 0,
+    "react/react-in-jsx-scope": "off",
+    "prettier/prettier": ["error"],
+  },
+  settings: {
+    react: {
+      version: "detect",
+    },
+  },
+};
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,12 @@
 function Header() {
-    return (
-        <header className="app-header">
-            <p className="app-header__description">Click the egg <em>15x</em> to help</p>
-            <h1 className="app-header__title">birb hatch</h1>
-        </header>
-    )
+  return (
+    <header className="app-header">
+      <p className="app-header__description">
+        Click the egg <em>15x</em> to help
+      </p>
+      <h1 className="app-header__title">birb hatch</h1>
+    </header>
+  );
 }
 
 export default Header;
